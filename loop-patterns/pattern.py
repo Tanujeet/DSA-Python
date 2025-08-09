ce998728707c9da4beca93c465028e98ebfa06de
Tanujeet <singhtanujeet@gmail.com>
2025-08-09 20:14:55 +0530

    Initial commit

--- loop-patterns/pattern.py
+++ loop-patterns/pattern.py
@@ -40,12 +40,12 @@ for i in range(5,0,-1):
 #patten 7
 
 for i in range(5):
-        for j in range(5,i,-1):
+        for j in range(4,i,-1):
             print(" ",end=" ")
         for j in range(2*i+1):
             print("*",end=" ") 
          
-print()
+        print()
 
 #pattern 8
 
@@ -56,6 +56,6 @@ for i in range(n):
      for j in range(2*(n-i)-1):
          print("*",end=" ") 
 
-print() 
+     print() 
 
 
