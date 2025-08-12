b1b60dc0f50858ed5291108b6ac860ecf346797c
Tanujeet <singhtanujeet@gmail.com>
2025-08-12 09:52:24 +0530

    Pattern practise

--- loop-patterns/pattern.py
+++ loop-patterns/pattern.py
@@ -208,3 +208,14 @@
 #    pattern192(n)
 #    print()
 # pattern19(5)   
+
+
+#pattern 21
+# n=4
+# for i in range(n):
+#     for j in range(n):
+#        if i == 0 or i == n-1 or j == 0 or j == n-1:
+#            print("*",end="")
+#        else:
+#            print(" ",end="")   
+#     print()
