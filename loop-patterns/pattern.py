5fc537381abe983d1f338166bdd3b5d2b704ec7c
Tanujeet <singhtanujeet@gmail.com>
2025-08-12 08:18:10 +0530

    Pattern practise

--- loop-patterns/pattern.py
+++ loop-patterns/pattern.py
@@ -29,3 +29,32 @@
 #     for j in range(1,i+1):
 #         print(i,end="")
 #     print()
+
+
+#pattern 5
+# n=5
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*",end="")
+#     print() 
+
+
+#pattern 6
+# n=5
+# for i in range(n):
+#     for j in range(1,n-i):
+#         print(j,end="")
+#     print()
+
+
+
+#pattern 7
+n=5
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(1,2*(i+1)):
+        print("*",end="")
+    for j in range(n-i):
+        print(" ",end="")
+    print()
