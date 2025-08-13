8f3a10db2e97ebae2f34994b85c177a1ef6bc5be
Tanujeet <singhtanujeet@gmail.com>
2025-08-13 08:31:24 +0530

    Pattern practise

--- loop-patterns/pattern.py
+++ loop-patterns/pattern.py
@@ -1,221 +1,26 @@
 #pattern 1
-# n=5 
-# for i in range(n):
-#     for j in range(n):
-#         print("*",end="")
-#     print()
+# def pattern1(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print("*",end="")
+#         print()
 
 
 
 #pattern 2
-# n=5
-# for i in range(n):
-#     for j in range(1,i+1):
-#         print("*",end="")
-#     print()
-
-
-#pattern 3
-# n=5
-# for i in range(n):
-#     for j in range(1,i+1):
-#         print(j,end="")
-#     print()
-
-
-#pattern 4
-# n=5
-# for i in range(n):
-#     for j in range(1,i+1):
-#         print(i,end="")
-#     print()
-
-
-#pattern 5
-# n=5
-# for i in range(n):
-#     for j in range(n-i):
-#         print("*",end="")
-#     print() 
-
-
-#pattern 6
-# n=5
-# for i in range(n):
-#     for j in range(1,n-i):
-#         print(j,end="")
-#     print()
+def pattern2(n):
+    for i in range(n):
+        print()
 
 
 
-#pattern 7
-# def pattern7(n):
-#     for i in range(n):
-#         for j in range(n-i):
-#             print(" ",end="")
-#         for j in range(1,2*(i+1)):
-#             print("*",end="")
-#         for j in range(n-i):
-#             print(" ",end="")
-#         print()
 
 
 
-#pattern 8
-# def pattern8(n):
-#     for i in range(n):
-#         for j in range(i):
-#             print(" ",end="")
-#         for j in range(2*(n-i)-1):
-#             print("*",end="")
-#         for j in range(i):
-#             print(" ",end="")
-      
-#         print()
-
-# pattern8(5)        
-
-#pattern 9
-# def pattern9(n):
-#     pattern7(n)
-#     pattern8(n)
-# pattern9(5)    
-
-
-#pattern 10
-# n=5 
-# for i in range(n):
-#     for j in range(i+1):
-#         print("*",end="")
-#     print()
-# for i in range(n-1):
-#     for j in range(n-i-1):
-#         print("*",end="")
-#     print()    
-
-
-#pattern 11
-
-# n=5
-# for i in range(n):
-#     start = 1 if i % 2 == 0 else 0
-#     for j in range(i+1):
-#         print(start,end="")
-#         start = 1 - start
-#     print()
-
-
-#pattern 12
-# n=5
-# for i in range(n):
-#     for j in range(1,i+1):
-#         print(j,end="")
-#     for j in range(1,2*(n-i)-1):
-#         print(" ",end="")
-#     for j in range(i,0,-1):
-#         print(j,end="")
-#     print()
-
-
-#pattern 13
-# n = 5  
-# num = 1 
-
-# for i in range(1, n + 1):
-#     for j in range(i):
-#         print(num, end=" ")
-#         num += 1
-#     print()
-
-
-#pattern 14
-# n=5
-# for i in range(n):
-#     for j in range(65,65+i):
-#         print(chr(j),end="")
-#     print()
-
-
-#pattern 15
-# n=5
-# for i in range(n,0,-1):
-#     for j in range(65,65+i):
-#         print(chr(j),end="")
-#     print()
-
-
-#pattern 16
-# n=5
-# for i in range(n):
-#   ch = chr(65+i)
-#   print(ch*(i+1))
-
-
-#pattern  17
-# n=5
-# for i in range(n):
-#     for j in range(n-i-1):
-#         print(" ",end="")
-#     for j in range(65,65+i):
-#         print(chr(j),end="")
-#     for j in range(65 + i - 2, 64, -1):
-#         print(chr(j), end="")
-
-#     for j in range(n-i-1):
-#         print(" ",end="")
-#     print()
-
-
-# pattern 19.1
-# def pattern191(n):
-#     for i in range(n):
-#         for j in range(i+1):
-#            print("*",end="")
-#         for j in range(2*(n-i)-2):
-#            print(" ",end="")
-#         for j in range(i+1):
-#            print("*",end="")
-#         print()
-
-
-#pattern 19.2
-# def pattern192(n):
-#    for i in range(n,0,-1):
-#       for j in range(i):
-#          print("*",end="")
-#       for j in range(2*(n-i)):
-#          print(" ",end="")
-#       for j in range(i):
-#          print("*",end="")
-#       print()
-  
 
 
 
-#pattern 19
-# def pattern19(n):
-#    pattern192(n)
-#    pattern191(n)
-#    print()
-# pattern19(5)   
 
-
-
-#pattern 20
-
-# def pattern19(n):
-#    pattern191(n)
-#    pattern192(n)
-#    print()
-# pattern19(5)   
-
-
-#pattern 21
-# n=4
-# for i in range(n):
-#     for j in range(n):
-#        if i == 0 or i == n-1 or j == 0 or j == n-1:
-#            print("*",end="")
-#        else:
-#            print(" ",end="")   
-#     print()
+def main(n):
+    pattern1(n)
+main(5)    
