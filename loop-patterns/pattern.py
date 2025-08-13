410e1acaf5c4ce12688f72deecc87adb88722bae
Tanujeet <singhtanujeet@gmail.com>
2025-08-13 09:19:22 +0530

    Pattern practise

--- loop-patterns/pattern.py
+++ loop-patterns/pattern.py
@@ -34,42 +34,73 @@
 
 
 #pattern 5
-def pattern5(n):
-     for i in range(n):
-          for j in range(n-i):
-               print("*",end="")
-          print()
+# def pattern5(n):
+#      for i in range(n):
+#           for j in range(n-i):
+#                print("*",end="")
+#           print()
 
 
 
 #pattern 6
-def pattern6(n):
-    for i in range(n):
-     
-        for j in range(n - i - 1):
-            print(" ", end="")
-    
-        for j in range(2 * i + 1):
-            print("*", end="")
-      
-        for j in range(n - i - 1):
-            print(" ", end="")
-        print()
+# def pattern6(n):
+#      for i in range(n+1):
+#           for j in range(n-i):
+#                print(j+1,end="")
+#           print()
+
+
+
 
 
 
 
 
 #pattern 7
-def pattern7(n):
-     for i in range(n):
-          for j in range(i):
-               print(" ",end="")
-          for j in range(2*(n-i)-1):
-               print("*",end="")
-          for j in range(i):
-               print(" ",end="")          
-          print()
+# def pattern6(n):
+#     for i in range(n):
+     
+#         for j in range(n - i - 1):
+#             print(" ", end="")
+    
+#         for j in range(2 * i + 1):
+#             print("*", end="")
+      
+#         for j in range(n - i - 1):
+#             print(" ", end="")
+#         print()
+
+
+
+
+
+#pattern 8
+# def pattern7(n):
+#      for i in range(n):
+#           for j in range(i):
+#                print(" ",end="")
+#           for j in range(2*(n-i)-1):
+#                print("*",end="")
+#           for j in range(i):
+#                print(" ",end="")          
+#           print()
+
+
+
+
+
+#pattern 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -83,7 +114,7 @@ def main(n):
     # pattern4(n)
     #  pattern5(n)
      pattern6(n)
-     pattern7(n)
+    #  pattern7(n)
     #  pattern8(n)
     #  pattern9(n)
 main(5)    
