f6061f8a09d2d071766223efca381aafebbf83c4
Tanujeet <singhtanujeet@gmail.com>
2025-08-14 08:47:46 +0530

    Basic math

--- loop-patterns/Maths.py
+++ loop-patterns/Maths.py
@@ -5,3 +5,17 @@ while(n>0):
     lastdigit = n % 10
     print(lastdigit)
     n = n//10
+ 
+  
+
+
+
+#count digits
+
+n = 7780
+count = 0
+while(n>0):
+    count =count +1
+    n = n//10
+    print("your digit count is :",count)
+  
