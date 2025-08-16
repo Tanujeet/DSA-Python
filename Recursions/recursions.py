7458f4aed79d0d5bf516c5792f3bd7e07c77e8f7
Tanujeet <singhtanujeet@gmail.com>
2025-08-16 21:10:20 +0530

    Started recursion

--- Recursions/recursions.py
+++ Recursions/recursions.py
@@ -1,11 +1,22 @@
 # Understand Recursion by printing something N times
-count = 0
-def f():
-    global count
-    if count >= 11:
-        return
-    print(count)
-    count+=1
-    f()
+
+# count = 0
+# def f():
+#     global count
+#     if count == 3:
+#         return
+#     print(count)
+#     count+=1
+#     f()
       
-f()
+# f()
+
+#Print your Name N times using recursion
+
+# def name(i,n):
+#     if i > n:
+#         return
+#     print("Harsh")
+#     name(i+1,n)
+# name(1,4)    
+
