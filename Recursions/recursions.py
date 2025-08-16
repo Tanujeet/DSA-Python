a36ae60574674a948f28aacc8da70f8ee412b6e8
Tanujeet <singhtanujeet@gmail.com>
2025-08-16 21:02:26 +0530

    Started recursion

--- Recursions/recursions.py
+++ Recursions/recursions.py
@@ -1,1 +1,11 @@
 # Understand Recursion by printing something N times
+count = 0
+def f():
+    global count
+    if count >= 11:
+        return
+    print(count)
+    count+=1
+    f()
+      
+f()
