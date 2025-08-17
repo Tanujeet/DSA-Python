1993d7eba22c1ff05f91c86dbe8dd7696a56af0d
Tanujeet <singhtanujeet@gmail.com>
2025-08-17 09:54:37 +0530

    Recursion 3 problems solved

--- Recursions/recursions.py
+++ Recursions/recursions.py
@@ -1,22 +1,32 @@
 # Understand Recursion by printing something N times
 
-# count = 0
-# def f():
-#     global count
-#     if count == 3:
-#         return
-#     print(count)
-#     count+=1
-#     f()
-      
-# f()
 
-#Print your Name N times using recursion
+# def recursion(i,n):
+#     if i == n:
+#         return
+#     print(i)
+#     recursion(i+1,n)
+# recursion(0,4)
+
+
+
+
+# Print your Name N times using recursion
 
 # def name(i,n):
-#     if i > n:
+#     while i == n:
 #         return
 #     print("Harsh")
 #     name(i+1,n)
-# name(1,4)    
+# name(0,5)    
 
+
+
+ # Print from N to 1 using Recursion
+# def reverse1(i,n):
+#     if i < 1:
+#         return
+#     print(i)
+#     reverse1(i-1,n)
+
+# reverse1(5,5)    
