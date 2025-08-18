e55c8db71b41548f6af7a34cec3b412de0295c45
Tanujeet <singhtanujeet@gmail.com>
2025-08-18 09:48:24 +0530

    Practised recursion

--- Recursions/recursions.py
+++ Recursions/recursions.py
@@ -34,4 +34,9 @@
 
 #Given a number ‘N’, find out the sum of the first N natural numbers.
 
-
+# def sum1(i,sum):
+#     if i < 1:
+#         print(sum)
+#         return
+#     sum1(i-1,sum+i)
+# sum1(3,0)    
