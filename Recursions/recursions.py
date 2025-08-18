f7d602ea362a5905c951b6bd49d28bb66944d86a
Tanujeet <singhtanujeet@gmail.com>
2025-08-18 09:43:53 +0530

    Practised recursion

--- Recursions/recursions.py
+++ Recursions/recursions.py
@@ -1,41 +1,37 @@
 # Understand Recursion by printing something N times
-
 # def recursion(i,n):
 #     if i == n:
 #         return
 #     print(i)
 #     recursion(i+1,n)
-# recursion(0,4)
+# recursion(0,4)        
+ 
 
 
 # Print your Name N times using recursion
 
-# def name(i,n):
-#     while i == n:
+# def recursion(i,n):
+#     if i == n:
 #         return
 #     print("Harsh")
-#     name(i+1,n)
-# name(0,5)    
-
+#     recursion(i+1,n)
+# recursion(0,4)        
+ 
 
 
 # Print from N to 1 using Recursion
 
-# def reverse1(i,n):
-#     if i < 1:
+
+# def recursion(i,n):
+#     if i < 1 :
 #         return
 #     print(i)
-#     reverse1(i-1,n)
-
-# reverse1(5,5)   
+#     recursion(i-1,n)
+# recursion(5,5)        
+ 
+   
 
 
 #Given a number ‘N’, find out the sum of the first N natural numbers.
 
 
-# def summation(i,sum):
-#     if i < 1:
-#         print(sum)
-#         return
-#     summation(i-1,sum+i)
-# summation(3,0)    
