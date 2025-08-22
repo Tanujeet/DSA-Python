5984953bd379a4279ab58def0e28742026301427
Tanujeet <singhtanujeet@gmail.com>
2025-08-22 09:40:49 +0530

    hashing

---
+++ Hashing/Hashing.py
@@ -0,0 +1,14 @@
+#  Given an array, we have found the number of occurrences of each element in the array.
+
+# def f(n,arr):
+#     mp={}
+#     for i in range(n):
+#         if arr[i] in mp:
+#             mp[arr[i]]+=1
+#         else:
+#             mp[arr[i]]=1
+#     for x in mp:
+#         print(x,mp[x])
+# arr=[10,5,10,15,10,5]
+# n=len(arr)
+# f(n,arr)                           
