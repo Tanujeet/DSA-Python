8c174534363c6ce86a3b2973f4e20170fe4a27e0
Tanujeet <singhtanujeet@gmail.com>
2025-08-24 20:17:28 +0530

    hashing

--- Hashing/Hashing.py
+++ Hashing/Hashing.py
@@ -1,14 +1,14 @@
 #  Given an array, we have found the number of occurrences of each element in the array.
 
-# def frequency(arr,n):
-#     mp={}
-#     for n in arr:
-#         mp[n] = mp.get(n,0)+1
-#     print(mp)    
+def frequency(arr,n):
+    mp={}
+    for n in arr:
+        mp[n] = mp.get(n,0)+1
+    print(mp)    
         
-# arr=[10,5,15,10,5]
-# n=len(arr)
-# frequency(arr,n)
+arr=[10,5,15,10,5]
+n=len(arr)
+frequency(arr,n)
 
 
 # Given an array of size N. Find the highest and lowest frequency element.
