c4134ab1dbd8171b7e946888762a3e4ea4e92290
Tanujeet <singhtanujeet@gmail.com>
2025-08-27 20:44:39 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -1,15 +1,15 @@
 # Largest Element in an Array
 
-def f(arr):
-    largest = arr[0]
-    n = len(arr)
-    for i in range(n):
-        if arr[i] > largest:
-            largest = arr[i]
-    return largest
+# def f(arr):
+#     largest = arr[0]
+#     n = len(arr)
+#     for i in range(n):
+#         if arr[i] > largest:
+#             largest = arr[i]
+#     return largest
 
-arr = [1,2,4,5,1]
-print(f(arr))        
+# arr = [1,2,4,5,1]
+# print(f(arr))        
 
 
 # second largest element in the array 
