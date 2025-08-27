8c06c57298aafa4792c24a8f065f73032c2562cc
Tanujeet <singhtanujeet@gmail.com>
2025-08-27 21:01:34 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -58,16 +58,31 @@
 
 # Check if the array is sorted
 
-def is_sorted(arr):
-    n = len(arr)
-    for i in range(1,n):
-        if arr[i] <= arr[i-1]:
-            return False
-    return True         
+# def is_sorted(arr):
+#     n = len(arr)
+#     for i in range(1,n):
+#         if arr[i] <= arr[i-1]:
+#             return False
+#     return True         
 
 
 
-print(is_sorted([1, 2, 3, 4]))   
-print(is_sorted([1, 5, 3, 4]))   
-print(is_sorted([10]))          
-print(is_sorted([])) 
+# print(is_sorted([1, 2, 3, 4]))   
+# print(is_sorted([1, 5, 3, 4]))   
+# print(is_sorted([10]))          
+# print(is_sorted([])) 
+
+
+# Remove duplicates from Sorted array
+
+#brute approach
+
+# def f(arr):
+#     n =len(arr)
+#     st = set()
+#     for i in range(n):
+#         st.add(arr[i])
+#     return st    
+
+# arr =[1,1,2,2,2,3,3]
+# print(f(arr))
