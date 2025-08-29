31c5380356271ad6f4d9248e703d17b19f9d2732
Tanujeet <singhtanujeet@gmail.com>
2025-08-29 10:26:34 +0530

    Practising: Sorting

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -149,19 +149,19 @@ print("Total Swaps:", swaps)
 
 # insertion sort
 
-# def f(arr):
-#     n= len(arr)
-#     for i in range(n):
-#         for j in range(i,0,-1):
-#             if arr[j-1] > arr[j]:
-#                 arr[j-1],arr[j]= arr[j],arr[j-1]
-#             else:
-#                 break
-#     return arr        
+def f(arr):
+    n= len(arr)
+    for i in range(n):
+        for j in range(i,0,-1):
+            if arr[j-1] > arr[j]:
+                arr[j-1],arr[j]= arr[j],arr[j-1]
+            else:
+                break
+    return arr        
 
 
-# arr= [-5,3,2,1,-3,-3,7,2,2]           
-# print(f(arr))     
+arr= [-5,3,2,1,-3,-3,7,2,2]           
+print(f(arr))     
 
 
 
