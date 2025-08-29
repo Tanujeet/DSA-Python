958537dd3f9668fee9342632a4ec36db7428c01d
Tanujeet <singhtanujeet@gmail.com>
2025-08-29 10:26:09 +0530

    Practising: Sorting

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -91,18 +91,18 @@ print(bubble_sort(arr))
 
 #descending order
 
-# def bubble_sort(arr):
-#     n = len(arr)
-#     for i in range(n):
-#         for j in range(0, n-i-1):
-#             if arr[j] < arr[j+1]:
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
-#     return arr
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] < arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
 
 
-# arr= [-5,3,2,1,-3,-3,7,2,2]       
-# print(bubble_sort(arr))
+arr= [-5,3,2,1,-3,-3,7,2,2]       
+print(bubble_sort(arr))
 
 
 # check if array is already sorted or not 
