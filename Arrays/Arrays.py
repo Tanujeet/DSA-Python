27b1aa5784b0132c6eea3ce134a38c7ff530ead4
Tanujeet <singhtanujeet@gmail.com>
2025-08-31 20:58:12 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -192,20 +192,23 @@
 
 # Find the Union sorted arrays
 
-# def f(arr1,arr2):
-#     union =set()
-#     for x in arr1:
-#         union.add(x)
-#     for y in arr2:
-#         union.add(y)
 
-#     return sorted(list(union))         
+#brute force
 
-# arr1=[1,1,2,3,4,5]
-# arr2=[2,3,4,4,5,6]
-# print(f(arr1,arr2))
+def f(arr1,arr2):
+    union =set()
+    for x in arr1:
+        union.add(x)
+    for y in arr2:
+        union.add(y)
+
+    return sorted(list(union))         
+
+arr1=[1,1,2,3,4,5]
+arr2=[2,3,4,4,5,6]
+print(f(arr1,arr2))
                  
         
-
+#optimal approach
 
     
