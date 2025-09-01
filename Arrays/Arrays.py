2201329c10acfbf0b184c4a3cdeb7d34ba971540
Tanujeet <singhtanujeet@gmail.com>
2025-09-01 10:25:12 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -248,3 +248,29 @@
 
 
 #intersection of sorted Arrays
+
+
+# def intersection(arr1,arr2):
+#     n = len(arr1)
+#     m = len(arr2)
+#     i = 0
+#     j = 0
+#     ans=[]
+#     while i < n and j < m:
+#         if arr1[i] < arr2[j]:
+#             i+=1
+#         elif arr2[j] <arr1[i]:
+#             j +=1
+
+#         else:
+#             ans.append(arr1[i])
+#             i+=1
+#             j+=1
+#     return ans
+
+
+# arr1=[1,2,2,1]
+# arr2=[2,2]
+# print(intersection(arr1,arr2))
+
+  
