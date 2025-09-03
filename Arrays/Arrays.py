a58c1057c44f977f9633e5829dc5f779f6343f15
Tanujeet <singhtanujeet@gmail.com>
2025-09-03 09:40:54 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -372,30 +372,31 @@
 
 
 #optimal 
-def longest_subarray_sum_k(arr, k):
-    n = len(arr)
-    prefix_sum = 0
-    mp = {}  # prefix_sum -> index
-    max_len = 0
 
-    for i in range(n):
-        prefix_sum += arr[i]
+# def longest_subarray_sum_k(arr, k):
+#     n = len(arr)
+#     prefix_sum = 0
+#     mp = {}  # prefix_sum -> index
+#     max_len = 0
 
-        # agar pura prefix hi k hai
-        if prefix_sum == k:
-            max_len = max(max_len, i + 1)
+#     for i in range(n):
+#         prefix_sum += arr[i]
 
-        # agar prefix_sum - k pehle aa chuka hai
-        if (prefix_sum - k) in mp:
-            max_len = max(max_len, i - mp[prefix_sum - k])
+#         # agar pura prefix hi k hai
+#         if prefix_sum == k:
+#             max_len = max(max_len, i + 1)
 
-        # sirf pehli baar store karte hain (shortest index rakho)
-        if prefix_sum not in mp:
-            mp[prefix_sum] = i
+#         # agar prefix_sum - k pehle aa chuka hai
+#         if (prefix_sum - k) in mp:
+#             max_len = max(max_len, i - mp[prefix_sum - k])
 
-    return max_len
+#         # sirf pehli baar store karte hain (shortest index rakho)
+#         if prefix_sum not in mp:
+#             mp[prefix_sum] = i
+
+#     return max_len
 
 
-arr = [2, 3, 5, 1, 9]
-k = 10
-print(longest_subarray_sum_k(arr, k))  # Output: 3 (subarray [2,3,5])
+# arr = [2, 3, 5, 1, 9]
+# k = 10
+# print(longest_subarray_sum_k(arr, k))  # Output: 3 (subarray [2,3,5])
