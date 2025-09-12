f40b7bcdfb3301d70b4a1bdc27978d780fd84b49
Tanujeet <singhtanujeet@gmail.com>
2025-09-12 20:09:11 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -564,21 +564,20 @@ def printSpiral(mat):
     bottom = n - 1
     right = m - 1
 
-    # Loop until all elements are not traversed.
+   
     while (top <= bottom and left <= right):
-        # For moving left to right
+     
         for i in range(left, right + 1):
             ans.append(mat[top][i])
 
         top += 1
 
-        # For moving top to bottom.
+      
         for i in range(top, bottom + 1):
             ans.append(mat[i][right])
 
         right -= 1
 
-        # For moving right to left.
         if (top <= bottom):
             for i in range(right, left - 1, -1):
                 ans.append(mat[bottom][i])
