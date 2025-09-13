b04cc2f04512e957069fb6c21b62611227e72a2d
Tanujeet <singhtanujeet@gmail.com>
2025-09-13 20:34:03 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -551,54 +551,54 @@ import math
 
 # Spiral Traversal of a Matrix
 
-def printSpiral(mat):
+# def printSpiral(mat):
     
-    ans = []
+#     ans = []
  
-    n = len(mat)
-    m = len(mat[0]) 
+#     n = len(mat)
+#     m = len(mat[0]) 
   
     
-    top = 0
-    left = 0
-    bottom = n - 1
-    right = m - 1
+#     top = 0
+#     left = 0
+#     bottom = n - 1
+#     right = m - 1
 
    
-    while (top <= bottom and left <= right):
+#     while (top <= bottom and left <= right):
      
-        for i in range(left, right + 1):
-            ans.append(mat[top][i])
+#         for i in range(left, right + 1):
+#             ans.append(mat[top][i])
 
-        top += 1
-
-      
-        for i in range(top, bottom + 1):
-            ans.append(mat[i][right])
-
-        right -= 1
-
-        if (top <= bottom):
-            for i in range(right, left - 1, -1):
-                ans.append(mat[bottom][i])
-
-            bottom -= 1
+#         top += 1
 
       
-        if (left <= right):
-            for i in range(bottom, top - 1, -1):
-                ans.append(mat[i][left])
+#         for i in range(top, bottom + 1):
+#             ans.append(mat[i][right])
 
-            left += 1
+#         right -= 1
 
-    return ans
+#         if (top <= bottom):
+#             for i in range(right, left - 1, -1):
+#                 ans.append(mat[bottom][i])
+
+#             bottom -= 1
+
+      
+#         if (left <= right):
+#             for i in range(bottom, top - 1, -1):
+#                 ans.append(mat[i][left])
+
+#             left += 1
+
+#     return ans
 
 
-mat = [[1, 2, 3, 4],
-       [5, 6, 7, 8],
-       [9, 10, 11, 12],
-       [13, 14, 15, 16]]
+# mat = [[1, 2, 3, 4],
+#        [5, 6, 7, 8],
+#        [9, 10, 11, 12],
+#        [13, 14, 15, 16]]
                      
-ans = printSpiral(mat)
+# ans = printSpiral(mat)
 
-print(ans)
+# print(ans)
