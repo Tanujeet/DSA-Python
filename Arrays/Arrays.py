fb18f81b78db602d5f2a5e382a76477a8cb9b47a
Tanujeet <singhtanujeet@gmail.com>
2025-09-16 21:00:38 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -669,7 +669,7 @@ import math
 
 #3sum
 
-def f(arr,target):
+def f(arr):
     ans=[]
     n=len(arr)
     arr.sort()
@@ -678,5 +678,26 @@ def f(arr,target):
         if i > 0 and arr[i]==arr[i-1]:
             continue
         j,k=i+1,n-1
+        while  j < k :
+            sum = arr[i]+arr[j]+arr[k]
+            if sum  < 0 :
+                j+=1
+            elif sum > 0:
+                k-=1
+            else:
+                temp=[arr[i],arr[j],arr[k]]
+                ans.append(temp)
+                j+=1
+                k-=1
+            while j < k  and arr[j]== arr[j-1]:
+                j+=1
+            while j < k and arr[k] == arr[k+1]:
+                k-=1
+    return ans                           
+
+
+arr=[-1,0,1,2,-1,-4]
+
+print(f(arr))
 
         
