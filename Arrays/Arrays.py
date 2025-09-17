345706300afa775e93e2bc7b100f8290d615780a
Tanujeet <singhtanujeet@gmail.com>
2025-09-17 21:04:58 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -702,3 +702,33 @@ import math
 
         
 #4sum
+
+class Solution:
+    def fourSum(nums,target):
+        n=len(nums)
+        ans=[]
+        nums.sort()
+        for i in range(n):
+            if i > 0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,n):
+                if j > i+1 and nums[j]==nums[j-1]:
+                    continue
+                k,l=j+1,n-1
+                while k < l:
+                    sum = nums[i]+nums[j]+nums[k]+nums[l]
+                    if sum == target:
+                        temp=[ nums[i],nums[j],nums[k],nums[l]]
+                        ans.append(temp)
+                        k+=1
+                        l-=1
+                        while k < l and nums[k]==nums[k-1]:
+                            k+=1
+                        while k < l  and nums[l]==nums[l+1]:
+                            l-=1
+                    elif sum < target:
+                        k+=1
+                    else:
+                        l-=1
+        return ans                              
+                
