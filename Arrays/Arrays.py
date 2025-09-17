f215de6220ca2cb181db69d44d2992ef796784a1
Tanujeet <singhtanujeet@gmail.com>
2025-09-17 21:08:47 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -171,7 +171,7 @@ import math
 #             arr[pos]=arr[i]
 #             pos +=1
 #     for i in range(pos,n):
-#         arr[i] =0
+#         arr[i] = 0
 #     return arr
 
 # arr=[0,0,1,0,12,3]            
