510233ecf88e8278a51935375cc931da88119c2c
Tanujeet <singhtanujeet@gmail.com>
2025-09-18 20:41:37 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -736,5 +736,22 @@ import math
 #Largest Subarray with 0 Sum
 
 
+def maxLen(A):
+    n=len(A)
+    mpp = {}
+    maxi = 0
+    sum = 0
+    for i in range(n):
+        sum += A[i]
+        if sum == 0:
+            maxi = i + 1
+        else:
+            if sum in mpp:
+                maxi = max(maxi, i - mpp[sum])
+            else:
+                mpp[sum] = i
+    return maxi
+
+
 
                 
