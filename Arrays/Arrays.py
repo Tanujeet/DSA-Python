1f9b19f8e253f2fabb14dd1b3d06c2239a68ac8b
Tanujeet <singhtanujeet@gmail.com>
2025-09-19 20:50:37 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -764,8 +764,8 @@ def f(arr):
     ans=[]
     arr.sort()
     for i in range(n):
-        if not ans or arr[i][0]>ans[-1][1]:
-            ans.append(arr[1])
+        if not ans or arr[i][0]  > ans[-1][1]:
+            ans.append(arr[i])
         else:    
             ans[-1][1]=max(ans[-1][1],arr[i][1])
     return ans
