b37d4049fbe3d4c3b6a49bb69a5251189120c7cc
Tanujeet <singhtanujeet@gmail.com>
2025-09-20 20:56:02 +0530

    Practising: Array questions

--- Arrays/Arrays.py
+++ Arrays/Arrays.py
@@ -783,4 +783,10 @@ def f(arr):
     hash = [0] * (n + 1) 
 
     for i in range(n):
-        hash[i]+=arr[i]
+        hash[arr[i]]+=1
+
+    repeating,missing=-1,-1
+    for i in range(1,n+1):
+        if hash[i]==2:
+            repeating=1
+            
