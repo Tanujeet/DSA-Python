3b653f63f18c89c51fb017f04ed70ea88ed3e71c
Tanujeet Singh <singhtanujeet@gmail.com>
2025-11-28 09:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.29%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for ch in s:
+            if ch != "]":
+                stack.append(ch)
+            else:
+                curr_str = ""
+                while stack[-1] != "[":
+                    curr_str = stack.pop() + curr_str 
+                stack.pop()
+
+
+                curr_num = ""
+                while stack and stack[-1].isdigit():
+                    curr_num=stack.pop()+curr_num
+
+
+                curr_str = int(curr_num) * curr_str
+                stack.append(curr_str) 
+
+        return "".join(stack)     
+        
