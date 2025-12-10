6b23f21e49525e8d49bca5dcee302a0fc10293c9
Tanujeet Singh <singhtanujeet@gmail.com>
2025-12-10 08:30:42 +0530

    Time: 282 ms (20.52%) | Memory: 31.3 MB (70.33%) - LeetSync

---
+++ 480-sliding-window-median/sliding-window-median.py
@@ -0,0 +1,20 @@
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+
+        li = SortedList()
+        ans = []
+
+        if  k == 1:
+            return nums
+
+        for i in range(len(nums)):
+            li.add(nums[i])
+
+            if len(li) > k:
+                li.remove(nums[i-k])
+
+            if len(li) == k:
+                median = li[k//2] if k%2==1 else (li[k//2]+li[k//2 -1])/2
+                ans.append(median)
+                
+        return ans           
