b4a634d1c6e0848380bb7b2ca9e20f5851c517bf
Tanujeet Singh <singhtanujeet@gmail.com>
2025-12-26 08:36:28 +0530

    Time: 51 ms (74.58%) | Memory: 18.2 MB (55.15%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+     
+        penalty = customers.count('Y')
+        min_penalty = penalty
+        best_time = 0
+
+      
+        for i, c in enumerate(customers):
+            if c == 'Y':
+                penalty -= 1   
+            else:  
+                penalty += 1  
+
+            if penalty < min_penalty:
+                min_penalty = penalty
+                best_time = i + 1
+
+        return best_time
