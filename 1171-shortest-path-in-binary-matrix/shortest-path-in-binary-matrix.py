4c76b093cb25adce1a4aa4702c31f57c6740bf63
Tanujeet Singh <singhtanujeet@gmail.com>
2026-02-13 10:09:24 +0530

    Time: 119 ms (91.43%) | Memory: 19.8 MB (60.75%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -4,7 +4,7 @@ class Solution:
 
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
-
+        directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
 
         q = deque([(0,0,1)])
 
@@ -18,19 +18,13 @@ class Solution:
             if r == n-1 and c == n -1 :
                 return d
 
-
-            for dr in range(-1,2):
-                for dc in range(-1,2):
-                    if dr == 0 and dc == 0:
-                        continue
-
-                    nr,nc = r + dr , c + dc
+            for dr , dc in directions:
+                nr,nc = r+ dr , c + dc
 
 
-                    if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
-                        grid[nr][nc] = 1
-                        q.append((nr,nc,d + 1))
+                if 0 <= nr < n and 0 <= nc < n  and grid[nr][nc] == 0:
+                    grid[nr][nc]=1
+                    q.append((nr,nc,d+1))
 
 
-        return -1                             
-        
+        return -1                 
